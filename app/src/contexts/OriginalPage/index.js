30b80680c0a489dd44cd0660fa3a03f5f238617c
streletss <xstreletssx@gmail.com>
2018-07-06 11:38:58 +0300

    add actions

--- app/src/contexts/OriginalPage/index.js
+++ app/src/contexts/OriginalPage/index.js
@@ -1,17 +1,46 @@
-import React, { createContext } from 'react'
+import React, { Component } from 'react'
 
-const Context = createContext()
-const { Provider, Consumer } = Context
+const defaultState = {
+  html: null,
+  activeElement: null,
+  pointedElement: null,
+  setActiveElement: () => {},
+  setPointedElement: () => {}
+}
+const Context = React.createContext(defaultState)
 
-const EnhancedProvider = props => {
-  return (
-    <Provider value={{ html: props.html }}>
-      { props.children }
-    </Provider>
-  )
+class EnhancedProvider extends Component {
+  constructor (props) {
+    super(props)
+    this.setActiveElement = this.setActiveElement.bind(this)
+    this.setPointedElement = this.setPointedElement.bind(this)
+
+    this.state = {
+      ...defaultState,
+      html: props.html,
+      setActiveElement: this.setActiveElement,
+      setPointedElement: this.setPointedElement
+    }
+  }
+
+  setPointedElement (element) {
+    this.setState({ pointedElement: element })
+  }
+
+  setActiveElement (element) {
+    this.setState({ activeElement: element })
+  }
+
+  render () {
+    return (
+      <Context.Provider value={this.state}>
+        { this.props.children }
+      </Context.Provider>
+    )
+  }
 }
 
 export default {
   Provider: EnhancedProvider,
-  Consumer
+  Consumer: Context.Consumer
 }
