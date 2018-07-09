a7c9e0e6173cd36cf09db25a12b5583bbde0448a
nstrelets <xstreletssx@gmail.com>
2018-07-09 18:44:38 +0300

    cleanup

--- client/app/src/components/containers/OriginalPage/index.js
+++ client/app/src/components/containers/OriginalPage/index.js
@@ -2,19 +2,8 @@ import React, { Component } from 'react'
 import OriginalPage from '@contexts/OriginalPage'
 import HtmlContent from '@presentational/HtmlContent'
 import ActiveElementHighlight from '@containers/ActiveElementHighlight'
-import { toImage } from '@utils/Domvas'
 
-export default class index extends Component {
-  componentDidMount () {
-    let elem = document.querySelector('.promo-text')
-    console.log('DUD', elem)
-    toImage(elem, (arg1, arg2, arg3) => {
-      console.log('arg1', arg1)
-      console.log('arg2', arg2)
-      console.log('arg3', arg3)
-    })
-  }
-
+class OriginalPageContainer extends Component {
   render () {
     return (
       <div>
@@ -31,3 +20,5 @@ export default class index extends Component {
     )
   }
 }
+
+export default OriginalPageContainer
